b3f7ae9fc2c345961761e8db79174dbda4aa863b
Robot-Inventor <30.keita@gmail.com>
2023-06-11 22:28:07 +0900

    1.1.0

--- userScript/ja.user.js
+++ userScript/ja.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shadowban Scanner (ja)
 // @namespace    https://github.com/Robot-Inventor/shadowban-scanner/
-// @version      1.0.0
+// @version      1.1.0
 // @description  A browser extension that detects shadowbans on Twitter.
 // @author       Robot-Inventor (ろぼいん / @keita_roboin)
 // @match        https://twitter.com/*
