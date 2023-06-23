0bf662d18e88ec8b7b226515d4712a55de45e0a9
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2023-06-23 09:49:01 +0900

    feat: add linting to GitHub Actions

--- script/package.js
+++ script/package.js
@@ -1,20 +1,5 @@
 const { execSync } = require("child_process");
-
-const IGNORE_FILES = [
-    "./src/",
-    "./script/",
-    "./node_modules/",
-    "./package.json",
-    "./package-lock.json",
-    "./*.md",
-    "./manifest/",
-    "./web-ext-artifacts/",
-    "./userScript/",
-    "./renovate.json",
-    "./tsconfig.json",
-    "./webpack.config.js",
-    "./doc/"
-].map((path) => `"${path}"`).join(" ");
+const { WEB_EXT_IGNORE_FILES } = require("./settings.js");
 
 console.log("Building...");
 execSync("npm run build");
@@ -22,10 +7,10 @@ execSync("npm run build");
 console.log("Switching to manifest v2.");
 execSync("node ./script/changeManifestVersion.js 2");
 console.log("Packaging...");
-execSync(`npx web-ext build --artifacts-dir "./web-ext-artifacts/manifestV2" --ignore-files ${IGNORE_FILES}`);
+execSync(`npx web-ext build --artifacts-dir "./web-ext-artifacts/manifestV2" --ignore-files ${WEB_EXT_IGNORE_FILES}`);
 
 console.log("Switching to manifest v3.");
 execSync("node ./script/changeManifestVersion.js 3");
 console.log("Packaging...");
-execSync(`npx web-ext build --artifacts-dir "./web-ext-artifacts/manifestV3" --ignore-files ${IGNORE_FILES}`);
+execSync(`npx web-ext build --artifacts-dir "./web-ext-artifacts/manifestV3" --ignore-files ${WEB_EXT_IGNORE_FILES}`);
 console.log("Done.");
