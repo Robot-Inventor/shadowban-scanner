f0733c913d5d21139dcc78fce63ba0cead1c0307
Robot-Inventor <30.keita@gmail.com>
2023-07-11 01:52:09 +0900

    fix: improve the mechanism to determine if a tweet is by the current user #60

---
+++ src/ts/core/reactProps.guard.ts
@@ -0,0 +1,160 @@
+/*
+ * Generated type guards for "reactProps.ts".
+ * WARNING: Do not manually change this file.
+ */
+import { MenuBarReactPropsData, TweetOuterReactPropsData, FocalTweetOuterReactPropsData, ProfileReactPropsData } from "./reactProps";
+
+export function isMenuBarReactPropsData(obj: unknown): obj is MenuBarReactPropsData {
+    const typedObj = obj as MenuBarReactPropsData
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        Array.isArray(typedObj["children"]) &&
+        (typedObj["children"][1] !== null &&
+            typeof typedObj["children"][1] === "object" ||
+            typeof typedObj["children"][1] === "function") &&
+        (typedObj["children"][1]["props"] !== null &&
+            typeof typedObj["children"][1]["props"] === "object" ||
+            typeof typedObj["children"][1]["props"] === "function") &&
+        (typedObj["children"][1]["props"]["retweetWithCommentLink"] !== null &&
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"] === "object" ||
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"] === "function") &&
+        (typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"] !== null &&
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"] === "object" ||
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"] === "function") &&
+        (typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"] !== null &&
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"] === "object" ||
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"] === "function") &&
+        (typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive"] === "undefined" ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive"] === null ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive"] === false ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive"] === true) &&
+        (typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive_editable"] === "undefined" ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive_editable"] === null ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive_editable"] === false ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["possibly_sensitive_editable"] === true) &&
+        (typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"] !== null &&
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"] === "object" ||
+            typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"] === "function") &&
+        (typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"]["possibly_sensitive"] === "undefined" ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"]["possibly_sensitive"] === null ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"]["possibly_sensitive"] === false ||
+            typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"]["possibly_sensitive"] === true) &&
+        typeof typedObj["children"][1]["props"]["retweetWithCommentLink"]["state"]["quotedStatus"]["user"]["screen_name"] === "string"
+    )
+}
+
+export function isTweetOuterReactPropsData(obj: unknown): obj is TweetOuterReactPropsData {
+    const typedObj = obj as TweetOuterReactPropsData
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        Array.isArray(typedObj["children"]) &&
+        Array.isArray(typedObj["children"][0]) &&
+        (typedObj["children"][0][1] !== null &&
+            typeof typedObj["children"][0][1] === "object" ||
+            typeof typedObj["children"][0][1] === "function") &&
+        (typedObj["children"][0][1]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"] === "function") &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"]) &&
+        (typedObj["children"][0][1]["props"]["children"][0] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"] === "function") &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"][0]["props"]["children"]) &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"] === "function") &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"]) &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1]) &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"] === "function") &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"]) &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1]["props"] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1]["props"]["loggedInUser"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1]["props"]["loggedInUser"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1]["props"]["loggedInUser"] === "function") &&
+        typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][1]["props"]["children"][1][2]["props"]["children"][1]["props"]["loggedInUser"]["screen_name"] === "string"
+    )
+}
+
+export function isFocalTweetOuterReactPropsData(obj: unknown): obj is FocalTweetOuterReactPropsData {
+    const typedObj = obj as FocalTweetOuterReactPropsData
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        Array.isArray(typedObj["children"]) &&
+        Array.isArray(typedObj["children"][0]) &&
+        (typedObj["children"][0][1] !== null &&
+            typeof typedObj["children"][0][1] === "object" ||
+            typeof typedObj["children"][0][1] === "function") &&
+        (typedObj["children"][0][1]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"] === "function") &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"]) &&
+        (typedObj["children"][0][1]["props"]["children"][0] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"] === "function") &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"][0]["props"]["children"]) &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"] === "function") &&
+        Array.isArray(typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"]) &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6]["props"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6]["props"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6]["props"] === "function") &&
+        (typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6]["props"]["loggedInUser"] !== null &&
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6]["props"]["loggedInUser"] === "object" ||
+            typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6]["props"]["loggedInUser"] === "function") &&
+        typeof typedObj["children"][0][1]["props"]["children"][0]["props"]["children"][2]["props"]["children"][6]["props"]["loggedInUser"]["screen_name"] === "string"
+    )
+}
+
+export function isProfileReactPropsData(obj: unknown): obj is ProfileReactPropsData {
+    const typedObj = obj as ProfileReactPropsData
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        Array.isArray(typedObj["children"]) &&
+        (typedObj["children"][1] !== null &&
+            typeof typedObj["children"][1] === "object" ||
+            typeof typedObj["children"][1] === "function") &&
+        (typedObj["children"][1]["props"] !== null &&
+            typeof typedObj["children"][1]["props"] === "object" ||
+            typeof typedObj["children"][1]["props"] === "function") &&
+        (typedObj["children"][1]["props"]["user"] !== null &&
+            typeof typedObj["children"][1]["props"]["user"] === "object" ||
+            typeof typedObj["children"][1]["props"]["user"] === "function") &&
+        (typedObj["children"][1]["props"]["user"]["possibly_sensitive"] === null ||
+            typedObj["children"][1]["props"]["user"]["possibly_sensitive"] === false ||
+            typedObj["children"][1]["props"]["user"]["possibly_sensitive"] === true)
+    )
+}
--- src/ts/core/reactProps.ts
+++ src/ts/core/reactProps.ts
@@ -1,3 +1,138 @@
+import {
+    isMenuBarReactPropsData,
+    isProfileReactPropsData,
+    isTweetOuterReactPropsData,
+    isFocalTweetOuterReactPropsData
+} from "./reactProps.guard";
+
+/** @see {isMenuBarReactPropsData} ts-auto-guard:type-guard */
+export interface MenuBarReactPropsData {
+    children: [
+        unknown,
+        {
+            props: {
+                retweetWithCommentLink: {
+                    state: {
+                        quotedStatus: TweetReactPropsData;
+                    };
+                };
+            };
+        }
+    ];
+}
+
+/** @see {isTweetOuterReactPropsData} ts-auto-guard:type-guard */
+export interface TweetOuterReactPropsData {
+    children: [
+        [
+            unknown,
+            {
+                props: {
+                    children: [
+                        {
+                            props: {
+                                children: [
+                                    unknown,
+                                    {
+                                        props: {
+                                            children: [
+                                                unknown,
+                                                [
+                                                    unknown,
+                                                    unknown,
+                                                    {
+                                                        props: {
+                                                            children: [
+                                                                unknown,
+                                                                {
+                                                                    props: {
+                                                                        loggedInUser: {
+                                                                            screen_name: string;
+                                                                        };
+                                                                    };
+                                                                }
+                                                            ];
+                                                        };
+                                                    }
+                                                ]
+                                            ];
+                                        };
+                                    }
+                                ];
+                            };
+                        }
+                    ];
+                };
+            }
+        ]
+    ];
+}
+
+/** @see {isFocalTweetOuterReactPropsData} ts-auto-guard:type-guard */
+export interface FocalTweetOuterReactPropsData {
+    children: [
+        [
+            unknown,
+            {
+                props: {
+                    children: [
+                        {
+                            props: {
+                                children: [
+                                    unknown,
+                                    unknown,
+                                    {
+                                        props: {
+                                            children: [
+                                                unknown,
+                                                unknown,
+                                                unknown,
+                                                unknown,
+                                                unknown,
+                                                unknown,
+                                                {
+                                                    props: {
+                                                        loggedInUser: {
+                                                            screen_name: string;
+                                                        };
+                                                    };
+                                                }
+                                            ];
+                                        };
+                                    }
+                                ];
+                            };
+                        }
+                    ];
+                };
+            }
+        ]
+    ];
+}
+
+interface TweetReactPropsData {
+    possibly_sensitive?: boolean | null;
+    possibly_sensitive_editable?: boolean | null;
+    user: {
+        possibly_sensitive?: boolean | null;
+        screen_name: string;
+    };
+}
+
+/** @see {isProfileReactPropsData} ts-auto-guard:type-guard */
+export interface ProfileReactPropsData {
+    children: [
+        unknown,
+        {
+            props: {
+                user: {
+                    possibly_sensitive: boolean | null;
+                };
+            };
+        }
+    ];
+}
+
 class ReactProps {
     private readonly element: Element;
     private readonly reactPropsName: string;
@@ -7,22 +142,45 @@ class ReactProps {
         this.reactPropsName = Object.getOwnPropertyNames(element).filter((n) => n.startsWith("__reactProps$"))[0];
     }
 
-    get() {
+    get(): unknown {
         // @ts-expect-error
-        return this.element[this.reactPropsName].children[1].props;
+        return this.element[this.reactPropsName];
     }
 }
 
 class TweetReactProps {
     private readonly tweet: Element;
+    private readonly menuBar: Element;
 
-    constructor(tweet: Element) {
+    constructor(tweet: Element, menuBar: Element) {
         this.tweet = tweet;
+        this.menuBar = menuBar;
     }
 
-    get() {
-        const reactProps = new ReactProps(this.tweet).get();
-        return reactProps.retweetWithCommentLink.state.quotedStatus;
+    get(): TweetReactPropsData {
+        const menuBarReactProps = new ReactProps(this.menuBar).get();
+        if (!isMenuBarReactPropsData(menuBarReactProps)) throw new Error("Type of menuBarReactProps is invalid.");
+        return menuBarReactProps.children[1].props.retweetWithCommentLink.state.quotedStatus;
+    }
+
+    get isTweetByCurrentUser(): boolean {
+        const tweetAuthorScreenName = this.get().user.screen_name;
+
+        const tweetReactProps = new ReactProps(this.tweet).get();
+        let currentUserScreenName = "";
+        if (isTweetOuterReactPropsData(tweetReactProps)) {
+            currentUserScreenName =
+                tweetReactProps.children[0][1].props.children[0].props.children[1].props.children[1][2].props
+                    .children[1].props.loggedInUser.screen_name;
+        } else if (isFocalTweetOuterReactPropsData(tweetReactProps)) {
+            currentUserScreenName =
+                tweetReactProps.children[0][1].props.children[0].props.children[2].props.children[6].props.loggedInUser
+                    .screen_name;
+        } else {
+            throw new Error("Type of tweetReactProps is invalid.");
+        }
+
+        return tweetAuthorScreenName === currentUserScreenName;
     }
 }
 
@@ -35,7 +193,8 @@ class ProfileReactProps {
 
     get() {
         const reactProps = new ReactProps(this.userName).get();
-        return reactProps;
+        if (!isProfileReactPropsData(reactProps)) throw new Error("Type of reactProps is invalid.");
+        return reactProps.children[1].props;
     }
 }
 
