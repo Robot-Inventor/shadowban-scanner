7c8afafe0418ff8cad31945d5ba398e3bb87bb0b
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2023-07-10 11:26:44 +0900

    feat: add a release note link to the version number label #56

--- src/ts/browserAction.ts
+++ src/ts/browserAction.ts
@@ -24,3 +24,4 @@ const { version } = browser.runtime.getManifest();
 const versionElement = document.getElementById("version-number");
 if (!versionElement) throw new Error("Failed to get #version-number element");
 versionElement.textContent = `v${version}`;
+versionElement.setAttribute("href", `https://github.com/Robot-Inventor/shadowban-scanner/releases/tag/v${version}`);
