fa92ed0ddb9a81cbfff5fbc3404bdf4c75189443
ろぼいん <50550561+Robot-Inventor@users.noreply.github.com>
2023-07-30 02:23:17 +0900

    fix: bug in display of messages to profiles does not follow settings #62 (#82)

--- src/ts/core/profileChecker.ts
+++ src/ts/core/profileChecker.ts
@@ -1,7 +1,8 @@
 import { Color } from "./color";
 import { MessageElement } from "./messageElement";
 import { ProfileReactProps } from "./reactProps";
-import { CHECKED_DATA_ATTRIBUTE } from "../common/settings";
+import { CHECKED_DATA_ATTRIBUTE, NO_PROBLEM_CLASS_NAME } from "../common/settings";
+import { MessageSummary } from "./messageType";
 
 class ProfileChecker {
     private readonly userName: Element;
@@ -14,11 +15,16 @@ class ProfileChecker {
         this.userName.setAttribute(CHECKED_DATA_ATTRIBUTE, "true");
         const reactProps = new ProfileReactProps(this.userName).get();
         const isUserPossiblySensitive = Boolean(reactProps.user.possibly_sensitive);
-        if (!isUserPossiblySensitive) return;
 
         const color = new Color().textColor;
 
-        const messageElement = new MessageElement({ type: "account", summary: "accountShadowbanned" }, color);
+        const messageElement = new MessageElement(
+            { type: "account", summary: MessageSummary.fromAccountStatus(isUserPossiblySensitive) },
+            color
+        );
+        if (!isUserPossiblySensitive) {
+            messageElement.element.classList.add(NO_PROBLEM_CLASS_NAME);
+        }
         const userDescription = document.querySelector("[data-testid='UserDescription']");
         if (!userDescription) throw new Error("Failed to get user description of profile");
         userDescription.insertAdjacentElement("afterend", messageElement.element);
