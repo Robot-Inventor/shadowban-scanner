0a701e4d1372e347e79531d2ae1bc1ce05424b68
ろぼいん <50550561+Robot-Inventor@users.noreply.github.com>
2023-10-20 13:50:17 +0900

    fix: convert changeManifestVersion.js to TypeScript #253 (#254)

--- script/changeManifestVersion.ts
+++ script/changeManifestVersion.ts
@@ -1,18 +1,18 @@
-const fs = require("fs");
+import fs from "fs";
 
-const packageJson = require("../package.json");
-const manifestV2 = require("../manifest/v2.json");
-const manifestV3 = require("../manifest/v3.json");
+import packageJson from "../package.json";
+import manifestV2 from "../manifest/v2.json";
+import manifestV3 from "../manifest/v3.json";
 
 const { version } = packageJson;
 const arg = process.argv[2];
 
-if (arg == 2) {
+if (arg == "2") {
     manifestV2.version = version;
     const manifestText = JSON.stringify(manifestV2, null, 4);
     fs.writeFileSync("./manifest.json", manifestText);
     console.log(`Switched to manifest v${arg}.`);
-} else if (arg == 3) {
+} else if (arg == "3") {
     manifestV3.version = version;
     const manifestText = JSON.stringify(manifestV3, null, 4);
     fs.writeFileSync("./manifest.json", manifestText);
