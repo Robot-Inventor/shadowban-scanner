e52639fdc07af7647f6d3c6d920cecb48dad32aa
ろぼいん <50550561+Robot-Inventor@users.noreply.github.com>
2023-10-24 13:12:18 +0900

    doc: add Twemoji to Open Source Licenses page #278 (#279)

--- src/ts/@types/ossLicenses.d.ts
+++ src/ts/@types/ossLicenses.d.ts
@@ -1,9 +1,9 @@
 /** @see {isOSSLicenses} ts-auto-guard:type-guard */
 export type OSSLicenses = Array<{
     name: string;
-    version: string;
+    version?: string;
     repository: string;
-    source: string;
+    source?: string;
     license: string;
     licenseText: string;
 }>;
