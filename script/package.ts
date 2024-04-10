b08e7810a56542bc016534427708d79c6dcc3acc
Robot-Inventor <30.keita@gmail.com>
2024-04-10 18:29:19 +0900

    fix: cannot pack the extension

--- script/package.ts
+++ script/package.ts
@@ -3,11 +3,11 @@ import { execSync } from "child_process";
 console.log("Building...");
 execSync("npm run build");
 
-console.log("Packaging...");
-process.chdir("chrome");
-execSync(`npx web-ext build --artifacts-dir "../web-ext-artifacts/manifestV2"`);
+console.log("Packaging for Chrome...");
+process.chdir("dist/chrome");
+execSync(`npx web-ext build --artifacts-dir "../../web-ext-artifacts/manifestV2"`);
 
-console.log("Packaging...");
+console.log("Packaging for Firefox...");
 process.chdir("../firefox");
-execSync(`npx web-ext build --artifacts-dir "../web-ext-artifacts/manifestV3"`);
+execSync(`npx web-ext build --artifacts-dir "../../web-ext-artifacts/manifestV3"`);
 console.log("Done.");
