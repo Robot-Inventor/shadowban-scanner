533b36ec3bd6edc5a455826c5dbe4e733c45fdaa
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2024-04-20 20:32:56 +0900

    fix: replace artifact filename with `short_name` (#641)

--- script/package.ts
+++ script/package.ts
@@ -5,11 +5,11 @@ execSync("npm run build");
 
 console.log("Packaging for Chrome...");
 execSync(
-    `npx web-ext build --source-dir "./dist/chrome/" --artifacts-dir "./web-ext-artifacts/manifestV3" --filename "shadowban_scanner-{version}-manifestV3.zip"`
+    `npx web-ext build --source-dir "./dist/chrome/" --artifacts-dir "./web-ext-artifacts/manifestV3" --filename "{short_name}-{version}-manifestV3.zip"`
 );
 
 console.log("Packaging for Firefox...");
 execSync(
-    `npx web-ext build --source-dir "./dist/firefox/" --artifacts-dir "./web-ext-artifacts/manifestV2" --filename "shadowban_scanner-{version}-manifestV2.zip"`
+    `npx web-ext build --source-dir "./dist/firefox/" --artifacts-dir "./web-ext-artifacts/manifestV2" --filename "{short_name}-{version}-manifestV2.zip"`
 );
 console.log("Done.");
