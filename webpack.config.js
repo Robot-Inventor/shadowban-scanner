b79c32c3236b15714a8d995ef9bb14bd8717f70d
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2024-04-27 02:16:47 +0900

    refactor: change project structure (#647)
    * feat: use webpack
    
    * fix: update file path
    
    * fix: move favicon to `public` directory

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const CopyFilePlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        "js/script.js": "./src/js/script.js",
+        "js/en/script.js": "./src/js/en/script.js"
+    },
+    output: {
+        filename: "[name]",
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    plugins: [
+        new CopyFilePlugin({
+            patterns: [
+                {
+                    context: "./src/html/",
+                    from: "**/*",
+                    to: "./"
+                },
+                {
+                    context: "./src/css/",
+                    from: "**/*",
+                    to: "./css/"
+                },
+                {
+                    context: "./src/js/",
+                    from: "**/*",
+                    to: "./js/"
+                },
+                {
+                    context: "./public/",
+                    from: "**/*",
+                    to: "./"
+                }
+            ]
+        })
+    ]
+};
