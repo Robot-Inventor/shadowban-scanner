d41dfcf6e01494d32df05f9f7492895c18d9ad3c
Robot-Inventor <30.keita@gmail.com>
2024-04-27 02:43:46 +0900

    fix: fix ESLint errors

--- src/ts/en/script.ts
+++ src/ts/en/script.ts
@@ -1,7 +1,9 @@
 import Swal from "sweetalert2";
 
-// If the user came from a different website or directly accessed, and their browser language is Japanese,
-// redirect them to the Japanese version.
+/*
+ * If the user came from a different website or directly accessed, and their browser language is Japanese,
+ * redirect them to the Japanese version.
+ */
 if (
     (!document.referrer || new URL(document.referrer).hostname !== location.hostname) &&
     navigator.language.toLowerCase().startsWith("ja")
@@ -11,45 +13,47 @@ if (
 
 const buttons = document.querySelectorAll("button.download_button");
 
+// eslint-disable-next-line max-statements
 buttons.forEach((button) => {
     const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
     const isEdge = navigator.userAgent.toLowerCase().includes("edg");
 
-    let download_link = "https://chrome.google.com/webstore/detail/enlganfikppbjhabhkkilafmkhifadjd/";
-    let download_text = "Install to Chrome";
+    let downloadLink = "https://chrome.google.com/webstore/detail/enlganfikppbjhabhkkilafmkhifadjd/";
+    let downloadText = "Install to Chrome";
 
     if (isFirefox) {
-        download_link = "https://addons.mozilla.org/firefox/addon/shadowban-scanner/";
-        download_text = "Install to Firefox";
+        downloadLink = "https://addons.mozilla.org/firefox/addon/shadowban-scanner/";
+        downloadText = "Install to Firefox";
     } else if (isEdge) {
-        download_link =
+        downloadLink =
             "https://microsoftedge.microsoft.com/addons/detail/shadowban-scanner/kfeecmboomhggeeceipnbbdjmhjoccbl";
-        download_text = "Install to Edge";
+        downloadText = "Install to Edge";
     }
 
-    button.textContent = download_text;
+    button.textContent = downloadText;
 
     button.addEventListener("click", () => {
-        const isMobile = Boolean(navigator.userAgent.match(/iPhone|Android.+Mobile/));
+        const isMobile = Boolean(navigator.userAgent.match(/iPhone|Android.+Mobile/u));
         if (isMobile && !isFirefox) {
-            Swal.fire({
-                title: "Smartphones are not supported",
-                text: "Due to technical reasons, Shadowban Scanner is only available for PC browsers and Firefox for Android.",
-                icon: "warning",
-                showCancelButton: true,
+            void Swal.fire({
                 background: "#21272e",
+                cancelButtonColor: "#d33",
+                cancelButtonText: "Cancel",
                 color: "#fff",
                 confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
                 confirmButtonText: "Continue anyway",
-                cancelButtonText: "Cancel"
+                icon: "warning",
+                showCancelButton: true,
+                // eslint-disable-next-line max-len
+                text: "Due to technical reasons, Shadowban Scanner is only available for PC browsers and Firefox for Android.",
+                title: "Smartphones are not supported"
             }).then((result) => {
                 if (result.isConfirmed) {
-                    open(download_link, "_blank");
+                    open(downloadLink, "_blank");
                 }
             });
         } else {
-            open(download_link, "_blank");
+            open(downloadLink, "_blank");
         }
     });
 });
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,7 +1,9 @@
 import Swal from "sweetalert2";
 
-// If the user came from a different website or directly accessed, and their browser language is not Japanese,
-// redirect them to the English version.
+/*
+ * If the user came from a different website or directly accessed, and their browser language is not Japanese,
+ * redirect them to the English version.
+ */
 if (
     (!document.referrer || new URL(document.referrer).hostname !== location.hostname) &&
     !navigator.language.toLowerCase().startsWith("ja")
@@ -11,45 +13,46 @@ if (
 
 const buttons = document.querySelectorAll("button.download_button");
 
+// eslint-disable-next-line max-statements
 buttons.forEach((button) => {
     const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
     const isEdge = navigator.userAgent.toLowerCase().includes("edg");
 
-    let download_link = "https://chrome.google.com/webstore/detail/enlganfikppbjhabhkkilafmkhifadjd/";
-    let download_text = "Chromeにインストール";
+    let downloadLink = "https://chrome.google.com/webstore/detail/enlganfikppbjhabhkkilafmkhifadjd/";
+    let downloadText = "Chromeにインストール";
 
     if (isFirefox) {
-        download_link = "https://addons.mozilla.org/firefox/addon/shadowban-scanner/";
-        download_text = "Firefoxにインストール";
+        downloadLink = "https://addons.mozilla.org/firefox/addon/shadowban-scanner/";
+        downloadText = "Firefoxにインストール";
     } else if (isEdge) {
-        download_link =
+        downloadLink =
             "https://microsoftedge.microsoft.com/addons/detail/shadowban-scanner/kfeecmboomhggeeceipnbbdjmhjoccbl";
-        download_text = "Edgeにインストール";
+        downloadText = "Edgeにインストール";
     }
 
-    button.textContent = download_text;
+    button.textContent = downloadText;
 
     button.addEventListener("click", () => {
-        const isMobile = Boolean(navigator.userAgent.match(/iPhone|Android.+Mobile/));
+        const isMobile = Boolean(navigator.userAgent.match(/iPhone|Android.+Mobile/u));
         if (isMobile && !isFirefox) {
-            Swal.fire({
-                title: "スマートフォンには対応していません",
-                text: "Shadowban Scannerは技術的な理由により、PCのブラウザーとAndroid版Firefoxでのみ利用できます。",
-                icon: "warning",
-                showCancelButton: true,
+            void Swal.fire({
                 background: "#21272e",
+                cancelButtonColor: "#d33",
+                cancelButtonText: "キャンセル",
                 color: "#fff",
                 confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
                 confirmButtonText: "とにかく続行する",
-                cancelButtonText: "キャンセル"
+                icon: "warning",
+                showCancelButton: true,
+                text: "Shadowban Scannerは技術的な理由により、PCのブラウザーとAndroid版Firefoxでのみ利用できます。",
+                title: "スマートフォンには対応していません"
             }).then((result) => {
                 if (result.isConfirmed) {
-                    open(download_link, "_blank");
+                    open(downloadLink, "_blank");
                 }
             });
         } else {
-            open(download_link, "_blank");
+            open(downloadLink, "_blank");
         }
     });
 });
