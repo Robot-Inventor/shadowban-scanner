8925f04d402af0907d705285bf34286a456b50ee
Robot-Inventor <30.keita@gmail.com>
2024-08-07 00:56:31 +0900

    refactor: fix `Unexpected class with only static properties. eslint(@typescript-eslint/no-extraneous-class)` errors #901

--- src/ts/core/propsAnalyzer.ts
+++ src/ts/core/propsAnalyzer.ts
@@ -20,73 +20,70 @@ interface TweetAnalysisResult extends ProfileAnalysisResult {
     };
 }
 
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-class PropsAnalyzer {
-    public static analyzeProfileProps(props: UserProps): ProfileAnalysisResult {
-        const possiblySensitive = Boolean(props.possibly_sensitive);
-        const sensitiveMediaInProfile = Boolean(
-            ["sensitive_media", "offensive_profile_content"].includes(props.profile_interstitial_type)
-        );
-        const withheldInCountries = props.withheld_in_countries;
-        const shadowbanned = possiblySensitive || sensitiveMediaInProfile;
+const analyzeProfileProps = (props: UserProps): ProfileAnalysisResult => {
+    const possiblySensitive = Boolean(props.possibly_sensitive);
+    const sensitiveMediaInProfile = Boolean(
+        ["sensitive_media", "offensive_profile_content"].includes(props.profile_interstitial_type)
+    );
+    const withheldInCountries = props.withheld_in_countries;
+    const shadowbanned = possiblySensitive || sensitiveMediaInProfile;
 
-        return {
-            user: {
-                // eslint-disable-next-line no-magic-numbers
-                hasAnyProblem: shadowbanned || withheldInCountries.length > 0,
-                sensitiveMediaInProfile,
-                shadowbanned,
-                withheldInCountries
-            }
-        };
-    }
-
-    public static analyzeTweetProps(parser: TweetParser): TweetAnalysisResult {
-        const props = parser.parse();
-
-        const userAnalysisResult = PropsAnalyzer.analyzeProfileProps(props.user);
-        const possiblySensitive = Boolean(props.possibly_sensitive);
-        // Ref: https://github.com/Robot-Inventor/shadowban-scanner/issues/16
-        const possiblySensitiveEditable = !(props.possibly_sensitive_editable === false);
-
-        const ageRestriction = possiblySensitive && !possiblySensitiveEditable;
-
-        let searchability: TweetAnalysisResult["tweet"]["searchability"] = "searchable";
-
-        if (ageRestriction || userAnalysisResult.user.shadowbanned) {
-            searchability = "unsearchable";
-        } else if (possiblySensitive) {
-            searchability = "possiblyUnsearchable";
+    return {
+        user: {
+            // eslint-disable-next-line no-magic-numbers
+            hasAnyProblem: shadowbanned || withheldInCountries.length > 0,
+            sensitiveMediaInProfile,
+            shadowbanned,
+            withheldInCountries
         }
+    };
+};
 
-        return {
-            ...userAnalysisResult,
+const analyzeTweetProps = (parser: TweetParser): TweetAnalysisResult => {
+    const props = parser.parse();
 
-            tweet: {
-                ageRestriction,
-                hasAnyProblem: searchability !== "searchable" || userAnalysisResult.user.hasAnyProblem,
-                possiblySensitive,
-                searchability
-            }
-        };
+    const userAnalysisResult = analyzeProfileProps(props.user);
+    const possiblySensitive = Boolean(props.possibly_sensitive);
+    // Ref: https://github.com/Robot-Inventor/shadowban-scanner/issues/16
+    const possiblySensitiveEditable = !(props.possibly_sensitive_editable === false);
+
+    const ageRestriction = possiblySensitive && !possiblySensitiveEditable;
+
+    let searchability: TweetAnalysisResult["tweet"]["searchability"] = "searchable";
+
+    if (ageRestriction || userAnalysisResult.user.shadowbanned) {
+        searchability = "unsearchable";
+    } else if (possiblySensitive) {
+        searchability = "possiblyUnsearchable";
     }
 
-    public static analyzeTombstoneProps(props: [CellInnerDivProps, TombstoneGrandchildProps]): string | undefined {
-        const [cellInnerDivProps, grandchildProps] = props;
+    return {
+        ...userAnalysisResult,
 
-        // eslint-disable-next-line no-underscore-dangle, no-undefined
-        if (!cellInnerDivProps.children._owner) return undefined;
+        tweet: {
+            ageRestriction,
+            hasAnyProblem: searchability !== "searchable" || userAnalysisResult.user.hasAnyProblem,
+            possiblySensitive,
+            searchability
+        }
+    };
+};
 
-        // eslint-disable-next-line no-undefined
-        if (grandchildProps.children[0].props.entry.conversationPosition.showReplyContext) return undefined;
+const analyzeTombstoneProps = (props: [CellInnerDivProps, TombstoneGrandchildProps]): string | undefined => {
+    const [cellInnerDivProps, grandchildProps] = props;
 
-        // eslint-disable-next-line no-underscore-dangle
-        const { key } = cellInnerDivProps.children._owner;
-        // Extract tweet ID from `conversationthread-${string}-tweet-${string}`
-        // eslint-disable-next-line prefer-destructuring
-        const tweetId = key.split("-")[3];
-        return tweetId;
-    }
-}
+    // eslint-disable-next-line no-underscore-dangle, no-undefined
+    if (!cellInnerDivProps.children._owner) return undefined;
 
-export { ProfileAnalysisResult, TweetAnalysisResult, PropsAnalyzer };
+    // eslint-disable-next-line no-undefined
+    if (grandchildProps.children[0].props.entry.conversationPosition.showReplyContext) return undefined;
+
+    // eslint-disable-next-line no-underscore-dangle
+    const { key } = cellInnerDivProps.children._owner;
+    // Extract tweet ID from `conversationthread-${string}-tweet-${string}`
+    // eslint-disable-next-line prefer-destructuring
+    const tweetId = key.split("-")[3];
+    return tweetId;
+};
+
+export { ProfileAnalysisResult, TweetAnalysisResult, analyzeProfileProps, analyzeTweetProps, analyzeTombstoneProps };
