d19a482770f28ebd61619bfbe5f2b125cc53920e
Robot-Inventor <30.keita@gmail.com>
2024-11-07 11:09:28 +0900

    chore: add `./script/**/*.ts` and `./rspack.config.ts` to ESLint targets

--- script/addUserScriptsComment.ts
+++ script/addUserScriptsComment.ts
@@ -1,25 +1,39 @@
-import { glob } from "glob";
 import fs from "fs";
+import { glob } from "glob";
+import { isPlainObject } from "./util";
 import packagejson from "../package.json";
 import path from "path";
 
+// eslint-disable-next-line no-console
 console.log("Adding userScript comments...");
 const userScriptFiles = glob.sync("./userScript/*.user.js");
 
 for (const userScript of userScriptFiles) {
     const scriptString = fs.readFileSync(userScript, "utf-8");
+
     const languageCode = path.basename(userScript, ".user.js");
     const formattedLanguageCode = languageCode.toLowerCase().replace("_", "-");
     const languageName =
-        new Intl.DisplayNames([formattedLanguageCode], { type: "language" }).of(formattedLanguageCode) ||
+        new Intl.DisplayNames([formattedLanguageCode], { type: "language" }).of(formattedLanguageCode) ??
         formattedLanguageCode;
-    const localizedMessages = fs.readFileSync(`./src/_locales/${languageCode}/messages.json`, "utf-8");
+
+    const localizedMessages: unknown = JSON.parse(
+        fs.readFileSync(`./src/_locales/${languageCode}/messages.json`, "utf-8")
+    );
+    if (
+        !isPlainObject(localizedMessages) ||
+        !isPlainObject(localizedMessages.manifest_description) ||
+        typeof localizedMessages.manifest_description.message !== "string"
+    ) {
+        throw new Error(`Invalid localized messages for ${languageCode}`);
+    }
+
     const userScriptComment = `
 // ==UserScript==
 // @name         Shadowban Scanner (${languageName})
 // @namespace    https://github.com/Robot-Inventor/shadowban-scanner/
 // @version      ${packagejson.version}
-// @description  ${JSON.parse(localizedMessages)["manifest_description"]["message"]}
+// @description  ${localizedMessages.manifest_description.message}
 // @author       Robot-Inventor (ろぼいん / @keita_roboin)
 // @match        https://*.twitter.com/*
 // @match        https://*.x.com/*
@@ -35,8 +49,9 @@ for (const userScript of userScriptFiles) {
 // @grant        none
 // ==/UserScript==
 `.trim();
-    const newScriptString = userScriptComment + "\n\n" + scriptString;
+    const newScriptString = `${userScriptComment}\n\n${scriptString}`;
     fs.writeFileSync(userScript, newScriptString);
 }
 
+// eslint-disable-next-line no-console
 console.log("Done!");
--- script/addonsLinter.ts
+++ script/addonsLinter.ts
@@ -1,53 +1,63 @@
-import colors from "colors/safe";
+import * as colors from "colors/safe";
+import { type Options as LinterOptions, createInstance } from "addons-linter";
 import { execSync } from "child_process";
-import linter from "addons-linter";
 
-const main = async () => {
-    const linterConfigFirefox: linter.Options = {
+const ERROR_EXIT_CODE = 1;
+
+// eslint-disable-next-line max-statements
+const main = async (): Promise<void> => {
+    const linterConfigFirefox: LinterOptions = {
         config: {
+            // eslint-disable-next-line id-length
             _: ["./dist/firefox/"],
             logLevel: process.env.VERBOSE ? "debug" : "fatal",
-            shouldScanFile: (fileName, isDir) => {
-                return true;
-            }
+            shouldScanFile: () => true
         }
     };
 
-    const linterConfigChrome: linter.Options = {
+    const linterConfigChrome: LinterOptions = {
         config: {
             ...linterConfigFirefox.config,
+            // eslint-disable-next-line id-length
             _: ["./dist/chrome/"],
             enableBackgroundServiceWorker: true
         }
     };
 
-    const lintForFirefox = linter.createInstance(linterConfigFirefox);
-    const lintForChrome = linter.createInstance(linterConfigChrome);
+    const lintForFirefox = createInstance(linterConfigFirefox);
+    const lintForChrome = createInstance(linterConfigChrome);
 
+    // eslint-disable-next-line no-console
     console.log("Building...");
     execSync("npm run build");
 
+    // eslint-disable-next-line no-console
     console.log("Linting for Firefox...");
     const firefoxResult = await lintForFirefox.run();
     if (firefoxResult.errors.length) {
+        // eslint-disable-next-line no-console
         console.error(colors.red("Errors found when linting for Firefox."));
-        process.exit(1);
+        process.exit(ERROR_EXIT_CODE);
     }
 
+    // eslint-disable-next-line no-console
     console.log("Linting for Chrome...");
     const chromeResult = await lintForChrome.run();
     if (chromeResult.errors.length) {
+        // eslint-disable-next-line no-console
         console.error(colors.red("Errors found when linting for Chrome."));
-        process.exit(1);
+        process.exit(ERROR_EXIT_CODE);
     }
 
+    // eslint-disable-next-line no-console
     console.log("Done.");
 };
 
 try {
     void main();
 } catch (error) {
-    // @ts-expect-error
+    // @ts-expect-error error is not an instance of Error
+    // eslint-disable-next-line no-console, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     console.error(colors.red(error.stdout.toString()));
-    process.exit(1);
+    process.exit(ERROR_EXIT_CODE);
 }
