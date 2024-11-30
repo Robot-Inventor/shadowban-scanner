0b1480138e4c713b5f588acc819a3c537b26e35d
Robot-Inventor <30.keita@gmail.com>
2024-11-30 13:07:45 +0900

    fix: prevent settings screen width from exceeding viewport on mobile

--- src/ts/browserAction/browserAction.ts
+++ src/ts/browserAction/browserAction.ts
@@ -1,5 +1,10 @@
 import { loadSettings } from "./loadSettings";
 import { loadVersion } from "./loadVersion";
 
+const isMobile = Boolean(/iPhone|Android.+Mobile/u.exec(navigator.userAgent));
+if (isMobile) {
+    document.body.classList.add("mobile");
+}
+
 void loadSettings();
 loadVersion();
