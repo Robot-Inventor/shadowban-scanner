113445b04ac4eab850be261afe97f5f6ae59d72a
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2025-04-25 03:26:31 +0900

    fix: remove `tsx` from dependencies (#1492)

--- script/copyManifest.ts
+++ script/copyManifest.ts
@@ -1,8 +1,8 @@
 import fs from "fs";
 
-import manifestV2 from "../src/manifest/v2.json";
-import manifestV3 from "../src/manifest/v3.json";
-import packageJson from "../package.json";
+import manifestV2 from "../src/manifest/v2.json" with { type: "json" };
+import manifestV3 from "../src/manifest/v3.json" with { type: "json" };
+import packageJson from "../package.json" with { type: "json" };
 
 const { version } = packageJson;
 
