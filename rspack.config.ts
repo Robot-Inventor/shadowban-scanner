69bd2e7f3db6c16b4fd13e18e6713d0b79384b1f
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2026-03-15 16:13:56 +0900

    refactor: fix linter errors

--- rspack.config.ts
+++ rspack.config.ts
@@ -208,7 +208,7 @@ const unacceptableLicenseTest = (licenseIdentifier: string): boolean => {
     return !acceptableLicenses.includes(licenseIdentifier);
 };
 
-const isProduction = process.env["NODE_ENV"] === "production";
+const isProduction = process.env.NODE_ENV === "production";
 /* eslint-disable sort-keys */
 // eslint-disable-next-line max-lines-per-function
 const config = defineConfig((env) => ({
