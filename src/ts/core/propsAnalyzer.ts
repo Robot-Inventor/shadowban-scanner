0bf67d9637784a35b4ae5a87e41a39094d28d728
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2026-04-16 15:26:03 +0900

    feat: add support for `mediaVisibilityResults`

--- src/ts/core/propsAnalyzer.ts
+++ src/ts/core/propsAnalyzer.ts
@@ -53,8 +53,8 @@ const analyzeTweetProps = (parser: TweetParser): TweetAnalysisResult => {
     const possiblySensitive = Boolean(props.possibly_sensitive);
     // Ref: https://github.com/Robot-Inventor/shadowban-scanner/issues/16
     const possiblySensitiveEditable = !(props.possibly_sensitive_editable === false);
-
-    const ageRestriction = possiblySensitive && !possiblySensitiveEditable;
+    const hasBlurredImageInterstitialOpacity = typeof props.mediaVisibilityResults?.blurred_image_interstitial?.opacity === "number";
+    const ageRestriction = (possiblySensitive && !possiblySensitiveEditable) || hasBlurredImageInterstitialOpacity;
 
     let searchability: TweetAnalysisResult["tweet"]["searchability"] = "searchable";
 
